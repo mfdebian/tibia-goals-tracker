c2512d9ceb5595d8f9dbecec18db610216b42c6d
daydream <alfredo.arosteguy@gmail.com>
2022-09-07 22:18:20 -0300

    Updated dependencies

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Daily goals tracker title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Daily goals tracker/i);
   expect(linkElement).toBeInTheDocument();
 });
