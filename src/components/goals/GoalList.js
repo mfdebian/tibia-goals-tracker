358a35d3a67aa89fded7c47d387dc2daf35d84a1
daydream <alfredo.arosteguy@gmail.com>
2023-02-12 02:30:12 -0300

    wip: fetch data

--- src/components/goals/GoalList.js
+++ src/components/goals/GoalList.js
@@ -1,59 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
 import AddIcon from '@mui/icons-material/Add';
 import Goal from './Goal';
 import AddGoal from './AddGoal';
+import { allGoalsQuery, getData } from '../../firestore';
 
 function GoalList() {
-  
-  let someGoals = [
-    {
-      id: 1,
-      name: 'kill elf overseer',
-      daily: false,
-      completed: false,
-      triedTimes: 2,
-      amount: 2,
-      maxAmount: 5,
-      triedTimesToday: 0,
-    },
-    {
-      id: 2,
-      name: 'killing in the name of task points',
-      daily: false,
-      completed: true,
-      triedTimes: 100,
-      amount: 100,
-      maxAmount: 100,
-      triedTimesToday: 0,
-    },
-    {
-      id: 3,
-      name: 'tame panther',
-      daily: false,
-      completed: false,
-      triedTimes: 2930,
-      amount: 0,
-      maxAmount: 1,
-      triedTimesToday: 0,
-    },
-    {
-      id: 4,
-      name: 'kill scarlet',
-      daily: true,
-      completed: false,
-      triedTimes: 2930,
-      amount: 0,
-      maxAmount: 1,
-      triedTimesToday: 0,
-    },
 
-  ]
-
-  const [goals, setGoals] = useState(someGoals);
+  const [goals, setGoals] = useState([]);
   const [showAddGoalForm, setShowAddGoalForm] = useState(false);
+  
+  useEffect(()=> {
+    getData(allGoalsQuery)
+      .then(res => {
+        res.forEach(element => {
+          addGoal({id: element.id, ...element.data()});          
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
 
   const checkCompleted = (id) => {
     let updatedGoals = [...goals];
@@ -80,6 +50,7 @@ function GoalList() {
   }
 
   const addGoal = (goal) => {
+    console.log(goal);
     let updatedGoals = [...goals];
     updatedGoals.unshift(goal);
     setGoals(updatedGoals);
---
+++ src/firestore.js
@@ -0,0 +1,12 @@
+import { getFirestore } from "firebase/firestore";
+import { collection, query, getDocs } from "firebase/firestore";
+import { app } from './firebase';
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export const allGoalsQuery = query(collection(db, "goals"));
+
+export const getData = async (query) => {
+  return await getDocs(query);
+}
